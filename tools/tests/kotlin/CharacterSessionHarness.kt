6ace7265e6a67e16118fa173def343c6c912f571
Fury <81368760+FuryArchive@users.noreply.github.com>
2026-09-22 07:24:01 +0700

    test: migrate Kotlin harness namespaces

--- tools/tests/kotlin/CharacterSessionHarness.kt
+++ tools/tests/kotlin/CharacterSessionHarness.kt
@@ -1,6 +1,6 @@
-import com.dubl.character.android.data.InMemoryCharacterStore
-import com.dubl.character.android.model.*
-import com.dubl.character.android.state.CharacterSession
+import com.furybook.dubl.data.InMemoryCharacterStore
+import com.furybook.dubl.model.*
+import com.furybook.dubl.state.CharacterSession
 
 fun main() {
     val c = DublCharacter(id = "c1", attributes = AttributeId.entries.associateWith { AttributeValue(base = 2) })
