d2d55194e6f190ad198c9afb5b417a0b5a863090
Fury <81368760+FuryArchive@users.noreply.github.com>
2026-09-24 08:58:32 +0700

    refactor: remove direct FCP presentation dependency from Android resource screen

--- app/src/main/java/com/furybook/android/ui/screens/OverviewScreen.kt
+++ app/src/main/java/com/furybook/android/ui/screens/OverviewScreen.kt
@@ -97,10 +97,8 @@ import com.furybook.content.FcpUiAccentToken
 import com.furybook.content.FcpUiComponent
 import com.furybook.content.FcpUiHostOrder
 import com.furybook.content.FcpUiIconToken
-import com.furybook.content.FcpUiPresentation
 import com.furybook.content.FcpUiSurface
 import com.furybook.content.orderedUiItems
-import com.furybook.content.presentation
 import com.furybook.dubl.content.DublResourceMeterModel
 import com.furybook.dubl.content.DublResourceToggleModel
 import com.furybook.dubl.content.DublUiFeature
